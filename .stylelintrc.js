c10c59fbc819bef189634bec285a0f72e0685846
giotramu <14157578+giotramu@users.noreply.github.com>
2020-05-09 17:52:53 +0200

    feat(elements): neumorphic buttons

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,6 @@
 module.exports = {
   extends: '@giotramu/stylelint-config/scss',
   rules: {
-    //'font-weight-notation': ['numeric', {ignore: ['relative']}],
-    'font-weight-notation': null,
-    'declaration-property-value-blacklist': {
-      '/^border(?!-(width|spacing))/': [
-        /thin/,
-        /medium/,
-        /thick/,
-        '0' // prefer `none`
-      ]
-    }
+    'font-weight-notation': null
   }
 };
